d9d435c2e68d5e24a911ef668b4fba59d1958ce7
Juliana <julianarodriguez066@gmail.com>
2024-04-29 22:19:05 -0500

    Solve the problem with SecondFragment

--- app/src/main/java/com/example/utaskdeo/SecondFragment.kt
+++ app/src/main/java/com/example/utaskdeo/SecondFragment.kt
@@ -3,6 +3,8 @@ package com.example.utaskdeo
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
@@ -15,56 +17,62 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 class SecondFragment : Fragment() {
 
     private lateinit var addsBtn: FloatingActionButton
-    private lateinit var recv:RecyclerView
-    private lateinit var userList:ArrayList<UserData>
+    private lateinit var recv: RecyclerView
+    private lateinit var userList: ArrayList<UserData>
     private lateinit var userAdapter: UserAdapter
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_second)
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_second, container, false)
+
         /**set List*/
         userList = ArrayList()
+
         /**set find Id*/
-        addsBtn = findViewById(R.id.addingBtn)
-        recv = findViewById(R.id.mRecycler)
+        addsBtn = view.findViewById(R.id.addingBtn)
+        recv = view.findViewById(R.id.mRecycler)
+
         /**set Adapter*/
-        userAdapter = UserAdapter(this,userList)
+        userAdapter = UserAdapter(requireContext(), userList)
+
         /**setRecycler view Adapter*/
-        recv.layoutManager = LinearLayoutManager(this)
+        recv.layoutManager = LinearLayoutManager(requireContext())
         recv.adapter = userAdapter
+
         /**set Dialog*/
         addsBtn.setOnClickListener { addInfo() }
 
+        return view
     }
 
-
     private fun addInfo() {
-        val inflter = LayoutInflater.from(this)
-        val v = inflter.inflate(R.layout.item_add,null)
+        val inflater = LayoutInflater.from(requireContext())
+        val v = inflater.inflate(R.layout.item_add, null)
+
         /**set view*/
         val taskName = v.findViewById<EditText>(R.id.taskName)
         val description = v.findViewById<EditText>(R.id.taskDescription)
 
-        val addDialog = AlertDialog.Builder(this)
+        val addDialog = AlertDialog.Builder(requireContext())
 
         addDialog.setView(v)
-        addDialog.setPositiveButton("Ok"){
-                dialog,_->
+        addDialog.setPositiveButton("Ok") { dialog, _ ->
             val name = taskName.text.toString()
             val description = description.text.toString()
-            userList.add(UserData("Name: $name","Task : $description"))
+            userList.add(UserData("Name: $name", "Task : $description"))
             userAdapter.notifyDataSetChanged()
-            Toast.makeText(this,"Adding Task Information Success", Toast.LENGTH_SHORT).show()
+            Toast.makeText(requireContext(), "Adding Task Information Success", Toast.LENGTH_SHORT).show()
             dialog.dismiss()
         }
-        addDialog.setNegativeButton("Cancel"){
-                dialog,_->
+        addDialog.setNegativeButton("Cancel") { dialog, _ ->
             dialog.dismiss()
-            Toast.makeText(this,"Cancel", Toast.LENGTH_SHORT).show()
+            Toast.makeText(requireContext(), "Cancel", Toast.LENGTH_SHORT).show()
+        }
 
-        }
-        addDialog.create()
-        addDialog.show()
+        addDialog.create().show()
     }
-
 }
-
